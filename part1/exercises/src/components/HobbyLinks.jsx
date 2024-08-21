d3a1cd77b05022694bce590b0caa7d8198e33858
billkapes <kapes15@yahoo.com>
2024-08-21 00:01:23 -0500

    completed basic exercise

---
+++ part1/exercises/src/components/HobbyLinks.jsx
@@ -0,0 +1,13 @@
+export default function HobbyLinks() {
+    let hobbyLinks = ["https://www.amazon.com/", "https://www.iamats.com/" ]
+    return (
+        <>
+        <div>
+        <a href = {hobbyLinks[0]}>Amazon</a>
+        </div>
+        <div>
+        <a href = {hobbyLinks[1]}>L-Tek dance pads</a>
+        </div>
+        </>
+    )
+}
