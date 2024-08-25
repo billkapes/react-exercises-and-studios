c8c142d0d7f320a621b1cbd2d9bc7846f83564f0
billkapes <kapes15@yahoo.com>
2024-08-25 04:47:02 -0500

    completed studio

--- part2/studio/src/components/AuthorInfo.jsx
+++ part2/studio/src/components/AuthorInfo.jsx
@@ -1,5 +1,19 @@
+import recipedata from "./recipe.json";
+import "./styling.css";
+
 function AuthorInfo() {
-   return;
+  const recipeAuthor = <div>{recipedata[0].author}</div>;
+  const recipeAuthorImage = <img className="authorImage" src={recipedata[0].authorImage} alt={recipedata[0].author}></img>;
+  let url = recipedata[0].website;
+  const recipeWebsite = <a href={url} target="_blank">{url}</a>;
+  
+  return (
+    <div>
+       {recipeAuthorImage}
+       {recipeAuthor}
+       {recipeWebsite}
+    </div>
+ );
  }
  
  export default AuthorInfo;
